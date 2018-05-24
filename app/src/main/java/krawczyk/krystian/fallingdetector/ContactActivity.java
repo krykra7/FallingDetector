c14187b15782c0aca7711180561e8412f29087dc
Krystian Krawczyk <krystian.krawczyk@softiq.pl>
2018-05-24 15:28:07 +0200

    initial configuration

--- app/src/main/java/krawczyk/krystian/fallingdetector/ContactActivity.java
+++ app/src/main/java/krawczyk/krystian/fallingdetector/ContactActivity.java
@@ -13,18 +13,14 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener((View view) -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        );
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
